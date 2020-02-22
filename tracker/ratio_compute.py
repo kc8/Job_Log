7b33e2d1a738a926ca9950a217479c41e5a71a73
Kyle Cooper <kyle@cooperkyle.com>
2020-02-22 10:00:10 -0500

    Continued development and improvments

---
+++ tracker/ratio_compute.py
@@ -0,0 +1,12 @@
+# To-do: create a 'module' that displays the current stats for the job search. Example is below
+'''
+
+Stats
+37 applied and waiting to hear
+25 Denied
+2 interviews
+62 Total Applied for
+3% interview ratio
+0% offer
+40% Confirmed Denied
+'''
